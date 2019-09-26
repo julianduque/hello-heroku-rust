955e029bcc63a6166f0cd1c9e8749720c8c2067e
Julian Duque <julianduquej@gmail.com>
2019-09-26 14:37:37 -0700

    support stages

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -25,22 +25,30 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
+    let app_env = env::var("APP_ENV").unwrap_or_else(|_| "development".to_string());
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
+    let stage = b"GET /stage HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK \r\n\r\n", "index.html")
     } else if buffer.starts_with(sleep) {
         thread::sleep(Duration::from_millis(500));
         ("HTTP/1.1 200 OK \r\n\r\n", "index.html")
+    } else if buffer.starts_with(stage) {
+        ("HTTP/1.1 200 OK \r\n\r\n", app_env.as_str())
     } else {
         ("HTTP/1.1 404 NOT FOUND \r\n\r\n", "404.html")
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = match filename {
+        "index.html" | "404.html" => fs::read_to_string(filename).unwrap(),
+        _ => String::from(filename),
+    };
+
     let response = format!("{}{}", status_line, contents);
 
     stream.write(response.as_bytes()).unwrap();
